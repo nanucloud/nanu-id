de960c7539029809f6d6d093fec7647bcf20e490
0xC0FFE2 <github1@ncloud.sbs>
2025-01-22 03:08:35 +0900

    chore : add dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,27 +21,40 @@ configurations {
 	}
 }
 
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:2024.0.0")
+		mavenBom("com.google.cloud:libraries-bom:26.52.0")
+	}
+}
+
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+	implementation("io.github.openfeign:feign-okhttp")
+	implementation("com.squareup.okhttp3:okhttp:4.11.0")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.2")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
+	implementation("org.glassfish.jaxb:jaxb-runtime:2.3.1")
 	compileOnly("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
 	annotationProcessor("org.projectlombok:lombok")
+	runtimeOnly("com.mysql:mysql-connector-j")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("org.springframework.security:spring-security-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation(kotlin("stdlib-jdk8"))
-	implementation("org.springframework.boot:spring-boot-starter-data-redis")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-	implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("io.jsonwebtoken:jjwt:0.9.1")
 }
 
 kotlin {
