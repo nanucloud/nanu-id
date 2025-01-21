984ae2c6a26252cb486df2bb3777a3621f15983f
0xC0FFE2 <github1@ncloud.sbs>
2025-01-22 03:07:30 +0900

    chore : add exception code

--- src/main/kotlin/nanucloud/nanuid/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/nanucloud/nanuid/global/error/exception/ErrorCode.kt
@@ -6,8 +6,12 @@ enum class ErrorCode (
 ) {
     BAD_REQUEST(400,"Bad request"),
     INVALID_TOKEN(401,"Invalid token"),
-    EXPIRED_TOKEN(401,"expired token"),
+    EXPIRED_TOKEN(401,"Expired token"),
+    INVALID_RECAPTCHA_TOKEN(401,"Invalid recaptcha token"),
     USER_NOT_FOUND(404,"No User found"),
+    USER_ACCOUNT_LOCKED(423,"Account locked"),
+    USER_ACCOUNT_SUSPENDED(403,"Account suspended"),
+    ALREADY_EXISTING_USER(409,"Already existing user"),
     NO_PERMISSION(403,"No permission to access"),
     INTERNAL_SERVER_ERROR(500,"Server error occurred")
 }
