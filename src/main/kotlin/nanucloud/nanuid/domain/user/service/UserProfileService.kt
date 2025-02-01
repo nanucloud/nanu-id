d855553c752d4b32a99b1c616af9c4d16e715c62
0xC0FFE2 <github1@ncloud.sbs>
2025-02-02 06:31:47 +0900

    chore : 필요없는 패키지 제거 및 경로수정

--- src/main/kotlin/nanucloud/nanuid/domain/user/service/UserProfileService.kt
+++ src/main/kotlin/nanucloud/nanuid/domain/user/service/UserProfileService.kt
@@ -1,10 +1,10 @@
 package nanucloud.nanuid.domain.user.service
 
 import jakarta.transaction.Transactional
-import nanucloud.nanuid.domain.user.repository.UserRepository
+import nanucloud.nanuid.domain.user.persistence.repository.UserRepository
 import nanucloud.nanuid.domain.user.domain.User
-import nanucloud.nanuid.domain.user.dto.request.UserRegisterRequest
-import nanucloud.nanuid.domain.user.dto.response.UserProfileResponse
+import nanucloud.nanuid.domain.user.presentation.dto.request.UserRegisterRequest
+import nanucloud.nanuid.domain.user.presentation.dto.response.UserProfileResponse
 import nanucloud.nanuid.domain.user.exception.UserAlreadyExistsException
 import nanucloud.nanuid.domain.user.facade.UserFacade
 import org.springframework.beans.factory.annotation.Autowired
